fb9f0b45027a5cfaf118f805c1a548af3ca49497
Yoan Tournade <yoan@ytotech.com>
2015-12-11 16:14:49 +0100

    Use an object structure

--- radiation_watch.py
+++ radiation_watch.py
@@ -1,15 +1,43 @@
+"""
+Radiation Watch Pocket Geiger Type 5 library for Raspberry Pi.
+
+Documentation and usage at: https://github.com/MonsieurV/PiPocketGeiger
+
+Released under MIT License. See LICENSE file.
+
+Contributed by:
+- Radiation-watch.org <http://www.radiation-watch.org/>
+- Yoan Tournade <yoan@ytotech.com>
+"""
 import RPi.GPIO as GPIO
-GPIO.setmode(GPIO.BCM)
 
-GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+class RadiationWatch:
+    def __init__(self, radiationPin, noisePin):
+        self.radiationPin = radiationPin
+        self.noisePin = noisePin
 
-def onRadiation(c):
-	print("Raddon!")
+    def __enter__(self):
+        # Init the GPIO context.
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.radiationPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.setup(self.noisePin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        # Register local callbacks.
+        GPIO.add_event_detect(self.radiationPin, GPIO.FALLING,
+            callback=self._onRadiation)
+        GPIO.add_event_detect(self.noisePin, GPIO.FALLING,
+            callback=self._onNoise)
+        return self
 
-GPIO.add_event_detect(24, GPIO.FALLING, callback=onRadiation)
+    def __exit__(self, exc_type, exc_value, traceback):
+        GPIO.cleanup()
 
-try:
-	while(1):
-		pass
-finally:
-	GPIO.cleanup()
+    def _onRadiation(self, channel):
+        print("Ray appeared!")
+
+    def _onNoise(self, channel):
+        print("Vibration! Stop moving!")
+
+if __name__ == "__main__":
+    with RadiationWatch(24, 23) as radiationWatch:
+        while 1:
+            pass

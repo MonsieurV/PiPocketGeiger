a5a3c389aa0035af4b2d23237ec3a14249b484ac
Yoan Tournade <yoan@ytotech.com>
2015-12-12 20:04:45 +0100

    Use OAuth2 for the Google Docs example

--- examples/google_docs.py
+++ examples/google_docs.py
@@ -3,8 +3,9 @@
 """
 Log radiation levels to a Google Docs.
 
-Requires the gspread package:
+Requires the gspread and oauth2client packages:
     https://github.com/burnash/gspread
+    https://github.com/google/oauth2client
 You'll also need a Google Signed Credentials to access your Google account:
     http://gspread.readthedocs.org/en/latest/oauth2.html
 
@@ -16,12 +17,17 @@ from PiPocketGeiger import RadiationWatch
 import datetime
 import time
 import gspread
+import json
+from oauth2client.client import SignedJwtAssertionCredentials
 
-# Google Docs account email and password.
-GDOCS_EMAIL = 'example@gmail.com'
-GDOCS_PASSWORD = 'password'
-GDOCS_SPREADSHEET_NAME = 'Radiation_Laussou_France'
+# Google Docs account credentials.
+keys = json.load(open('gdocs_credentials_file.json'))
+scope = ['https://spreadsheets.google.com/feeds']
+credentials = SignedJwtAssertionCredentials(keys['client_email'],
+	keys['private_key'].encode(), scope)
+GDOCS_SPREADSHEET_NAME = 'Radiation_Spreadsheet'
 
+# Period for logging readings to Google Dosc, in seconds.
 LOGGING_PERIOD = 120
 
 if __name__ == "__main__":
@@ -33,14 +39,13 @@ if __name__ == "__main__":
             try:
                 status = radiationWatch.status()
                 print("Uploading... {0}".format(status))
-                gspread.login(GDOCS_EMAIL, GDOCS_PASSWORD).open(
+                gspread.authorize(credentials).open(
                     GDOCS_SPREADSHEET_NAME).sheet1.append_row((
                         datetime.datetime.now().strftime('%d/%m/%Y %H:%M:00'),
                         status[0],
                         status[1],
                         status[2],
                         status[3]))
-
                 print("Ok.")
             except Exception as e:
                 print(e)
